2fb3fdb8145ad83edc1c8ef9ef11c1ddfec081a2
rosemoengineering <35057450+rosemoengineering@users.noreply.github.com>
2018-02-16 23:11:16 +0200

    Add files via upload

---
+++ Lcd_temp_print/Lcd_temp_print.ino
@@ -0,0 +1,31 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <dht.h>
+
+// Set the LCD address 
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+dht DHT;
+
+#define DHT11_PIN 7
+void setup()
+{
+	
+  lcd.begin();// LCD ekranı balat
+  lcd.backlight();// Ekran arka ışığını aç
+  lcd.setCursor(0,0);
+  lcd.print("Hello, Rosemo!"); //İlk satıra yazalım
+  lcd.setCursor(3,1);
+  lcd.print("Engineering!"); //İkinci satıra yazalım
+}
+
+void loop()
+{
+   int chk = DHT.read11(DHT11_PIN);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Temperature = ");
+    lcd.setCursor(0,1);
+    lcd.print(DHT.temperature);
+    delay(1000);
+    
+}
